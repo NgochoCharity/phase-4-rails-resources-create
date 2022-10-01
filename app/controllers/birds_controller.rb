4e8a881605bfcaf7d0b8409e71722498201a78bc
Charity Wanjiku Ngocho <ngochocharity@gmail.com>
2022-10-01 03:50:00 +0300

    commit tree 859c6af5b952e47c616e907e77e0296154146b6a

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -16,4 +16,19 @@ class BirdsController < ApplicationController
     end
   end
 
+  # def create
+  #   bird = Bird.create(birds_params)
+  #   render json: bird, status: :created
+  # end
+
+  # private
+  # def birds_params
+  #   params.permit(:name, :species)
+  # end
+
+  def create
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created
+  end
+
 end
